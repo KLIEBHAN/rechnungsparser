d5522aa812b1aa23d77e70ebdd9c963a6c282566
Fabian Kliebhan <fabian.kliebhan@gmail.com>
2023-05-02 00:16:11 +0200

    fixes

--- main.py
+++ main.py
@@ -1,56 +1,117 @@
 import os
 import sys
-from invoice2data import extract_data
-from invoice2data.extract.loader import read_templates
 import tkinter as tk
 from tkinter import messagebox
 import PyPDF2
 import re
+import argparse
 from datetime import datetime
+import pysftp
+
+
+patterns = {
+    'date': r'Rechnungsdatum\s*(?:\/Lieferdatum)?\s*((\d{1,2}[-./]\d{1,2}[-./]\d{2,4})|(\d{1,2}\s+\w+\s+\d{2,4}))',
+    'invoice_number': r'(?:Rechnungsnummer|Rechnungs-Nr\.|Fakturanummer)\s*([A-Za-z0-9\-_]+)',
+    'amount': r'Zahlbetrag\s*([\d,.]+)\s*€',
+}
+remote_path_1 = '/C:/Daten/TATEX/Buchhaltung/2023/Buchungen/Rechnungen/2_Rechnungen_gebucht/'
+remote_path_2 = '/C:/Daten/TATEX/Buchhaltung/2023/Buchungen/Rechnungen/3_Rechnungen_abgeschloßen/'
+remote_server = 'VMTATEX'
+remote_username = "fabi"
+remote_password = "/85fabI-19!"
 
 def extract_text_from_pdf(pdf_path):
-    pdf_file = open(pdf_path, 'rb')
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
-    num_pages = len(pdf_reader.pages)
-
-    text = ''
-    for page in range(num_pages):
-        text += pdf_reader.pages[page].extract_text()
-
-    pdf_file.close()
+    with open(pdf_path, 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        num_pages = len(pdf_reader.pages)
+        text = ''.join(pdf_reader.pages[page].extract_text() for page in range(num_pages))
     return text
 
+
 def extract_invoice_data(pdf_path):
-    templates = read_templates('templates')
-    invoice_data = extract_data(pdf_path, templates=templates)
+    text = extract_text_from_pdf(pdf_path)
 
-    return invoice_data
+    # Extract date
+    match = re.search(patterns['date'], text, re.IGNORECASE)
+    if match:
+        invoice_date_str = match.group(1)
+        if '-' in invoice_date_str or '.' in invoice_date_str:
+            invoice_date = datetime.strptime(invoice_date_str, '%d.%m.%Y').date()
+        else:
+            invoice_date = datetime.strptime(invoice_date_str, '%d %B %Y').date()
+    else:
+        raise ValueError("Rechnungsdatum nicht gefunden.")
 
-def show_popup(message, title='Rechnungsdaten'):
-    root = tk.Tk()
-    root.withdraw()
-    messagebox.showinfo(title, message)
-    root.destroy()
+    # Extract invoice number
+    match = re.search(patterns['invoice_number'], text, re.IGNORECASE)
+    if match:
+        invoice_number = match.group(1)
+    else:
+        raise ValueError("Rechnungsnummer nicht gefunden.")
+
+    # Extract amount
+    match = re.search(patterns['amount'], text, re.IGNORECASE)
+    if match:
+        amount_str = match.group(1)
+    else:
+        raise ValueError("Betrag nicht gefunden.")
+
+    return {'date': invoice_date, 'invoice_number': invoice_number, 'amount': amount_str}
+
+
+def moveToServer(pdf_path,remote_path):
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None  # ignore host key checking
+
+    sftp = pysftp.Connection(remote_server, username=remote_username, password=remote_password, cnopts=cnopts)
+    sftp.put(pdf_path, remote_path)
+
+    sftp.close()
+
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Extract invoice data from a PDF file.')
+    parser.add_argument('pdf_path', type=str, help='path to the PDF file')
+    args = parser.parse_args()
 
-    if len(sys.argv) != 2:
-        print("Usage: python main.py /path/to/invoice.pdf")
-        sys.exit(1)
+    pdf_path = args.pdf_path
 
-    pdf_path = sys.argv[1]
-    if not os.path.exists(pdf_path):
-        print("Error: The specified PDF file does not exist.")
-        sys.exit(1)
+    if not pdf_path:
+        sys.exit("Es wurde keine PDF-Datei ausgewählt.")
 
-    pdftext = extract_text_from_pdf(pdf_path)
-    invoice_data = extract_invoice_data(pdf_path)
-    
-    if invoice_data:
-        invoice_date = invoice_data['date']
+    try:
+        invoice_data = extract_invoice_data(pdf_path)
+        invoice_date = invoice_data['date'].strftime('%Y_%m_%d')
+        invoice_date_german = invoice_data['date'].strftime('%d.%m.%Y')
         invoice_number = invoice_data['invoice_number']
-        formatted_date = invoice_date.strftime('%d.%m.%Y')
-        message = f"Rechnungsdatum: {formatted_date}\nRechnungsnummer: {invoice_number}"
-        show_popup(message)
-    else:
-        show_popup("Rechnungsdaten nicht gefunden.")
+        amount = invoice_data['amount']
+        message = f"Rechnungsdatum: {invoice_date_german}\nRechnungsnummer: {invoice_number}\nBetrag: {amount}€"
+        root = tk.Tk()
+        root.withdraw()
+        messagebox.showinfo("Rechnungsdaten", message)
+        new_file_name = f"{invoice_date}_{invoice_number}.pdf"
+
+        rename_file = messagebox.askyesno("Umbenennen?", "Möchten Sie die Datei in "+new_file_name+" umbenennen?")
+        if rename_file:
+            os.rename(pdf_path, new_file_name)
+            messagebox.showinfo("Erfolgreich","Erfolgreich umbenannt in "+new_file_name)
+
+        move_file = messagebox.askyesno("Verschieben?", "Möchten Sie die Datei "+pdf_path+" auf den Server verschieben?")
+
+
+        if move_file:
+            remote_path = ""
+            result = messagebox.askquestion(
+                "Pfad", "Welcher Remotepfad soll verwendert werden?\nYes -> "+ remote_path_1+"\nNo -> " +remote_path_2,type='yesno',
+                default='yes')
+            if result == 'yes':
+                remote_path = remote_path_1
+            else:
+                remote_path = remote_path_2
+            moveToServer(new_file_name,remote_path+pdf_path)
+            messagebox.showinfo("Erfolgreich","Erfolgreich hochgeladen")
+        root.destroy()
+
+    except ValueError as e:
+        sys.exit(f"Fehler: {str(e)}")
