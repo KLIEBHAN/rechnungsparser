3a56f5d3093fedac6ecffad6823c68ba9739427b
fabi <fabian.kliebhan@gmail.com>
2023-05-19 09:30:52 +0200

    fixes

--- main.py
+++ main.py
@@ -29,6 +29,14 @@ REMOTE_PASSWORD = "?"
 REMOTE_HTTP_URL = 'http://VMTATEX:3000/json'
 
 
+# Hilfsfunktion zum Erstellen eines Buttons
+def create_button(dialog, text, command):
+    """Erstellt einen Button."""
+    button = tk.Button(dialog, text=text, command=command)
+    button.pack(pady=(0, 3))
+
+
+# Funktion zur Extraktion von Text aus einer PDF-Datei
 def extract_text_from_pdf(pdf_path):
     """Extrahiert den Text aus einer PDF-Datei."""
     with open(pdf_path, 'rb') as pdf_file:
@@ -37,6 +45,7 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 
+# Funktion zur Extraktion von Rechnungsdaten aus Text
 def extract_invoice_data(text):
     """Extrahiert Rechnungsdaten aus dem Text."""
     invoice_data = {}
@@ -48,6 +57,8 @@ def extract_invoice_data(text):
             raise ValueError(f"{key} nicht gefunden.")
     return invoice_data
 
+
+# Funktion zum Parsen von Datumswerten aus einer Zeichenkette
 def parse_date(date_str):
     """Analysiert das Datum aus einer Zeichenkette."""
     try:
@@ -57,6 +68,7 @@ def parse_date(date_str):
     return parsed_date.date()
 
 
+# Kontextmanager zur Verwaltung von SFTP-Verbindungen
 @contextmanager
 def sftp_connection(server, username, password):
     """Öffnet eine SFTP-Verbindung."""
@@ -66,46 +78,42 @@ def sftp_connection(server, username, password):
         yield sftp
 
 
+# Funktion zum Verschieben einer Datei zum Server
 def move_to_server(pdf_path, remote_path):
     """Lädt PDF-Datei auf den Server."""
     with sftp_connection(REMOTE_SERVER, REMOTE_USERNAME, REMOTE_PASSWORD) as sftp:
         sftp.put(pdf_path, remote_path)
 
 
+# Funktion zur Auswahl zwischen zwei Optionen
 def choose_between_two_options(text, option1, option2):
     chosen_option = tk.StringVar()
 
     def set_option(option):
         chosen_option.set(option)
-        path_dialog.destroy()
+        dialog.destroy()
 
-    path_dialog = tk.Toplevel()
-    path_dialog.title(text)
+    dialog = tk.Toplevel()
+    dialog.title(text)
 
-    path_1_button = tk.Button(
-        path_dialog,
-        text=option1,
-        command=lambda: set_option(option1))
-    path_1_button.pack(pady=(0, 3))
+    create_button(dialog, option1, lambda: set_option(option1))
+    create_button(dialog, option2, lambda: set_option(option2))
 
-    path_2_button = tk.Button(
-        path_dialog,
-        text=option2,
-        command=lambda: set_option(option2))
-    path_2_button.pack(pady=(0, 3))
-
-    path_dialog.wait_window()
+    dialog.wait_window()
 
     while not chosen_option.get():
-        path_dialog.wait_window()
+        dialog.wait_window()
 
     return chosen_option.get()
 
 
+# Funktion zum Anzeigen von Informationen
 def show_info(titel, message):
     """Erstellen einer benutzerdefinierten Dialogbox"""
+
     def close_dialog():
         custom_dialog.destroy()
+
     custom_dialog = tk.Toplevel()
     custom_dialog.title(titel)
     custom_dialog.geometry("300x200")
@@ -121,6 +129,7 @@ def show_info(titel, message):
     custom_dialog.wait_window()
 
 
+# Funktion zum Anzeigen von Rechnungsdaten
 def show_invoice_data(invoice_data):
     """Zeigt die extrahierten Rechnungsdaten an."""
     invoice_date_german = invoice_data['date'].strftime('%d.%m.%Y')
@@ -133,12 +142,15 @@ def show_invoice_data(invoice_data):
     show_info("Rechnungsdaten", message)
 
 
+# Funktion zum Setzen des Betreffs
 def set_subject(invoice_data):
     invoice_data['subject'] = simpledialog.askstring(
         title="Betreff",
         prompt="Betreff eingeben.\t\t\t",
         initialvalue=f"{invoice_data['subject']}")
 
+
+# Funktion zum Umbenennen einer Datei
 def rename_file(pdf_path, invoice_data):
     """Benennt die Datei um und verschiebt sie."""
     invoice_date = invoice_data['date'].strftime('%Y_%m_%d')
@@ -152,6 +164,7 @@ def rename_file(pdf_path, invoice_data):
     messagebox.showinfo("Erfolgreich", f"Erfolgreich zu {invoice_date}_{invoice_number}.pdf umbenannt")
 
 
+# Funktion zum Verschieben einer Datei
 def move_file(invoice_data):
     remote_path = choose_between_two_options(
         "Remotepfad auswählen",
@@ -161,6 +174,8 @@ def move_file(invoice_data):
     move_to_server(invoice_data['new_file_name'], remote_path + invoice_data['new_file_name'])
     messagebox.showinfo("Erfolgreich", "Erfolgreich hochgeladen")
 
+
+# Funktion zum Senden von Rechnungsdaten an einen Server
 def post_invoice_data(invoice_data, datum, hinbuchung):
     """Sendet die Rechnungsdaten an einen Server."""
 
@@ -170,7 +185,7 @@ def post_invoice_data(invoice_data, datum, hinbuchung):
         "Amazon Bürobedarf")
 
     if hinbuchung:
-        if(rechnungstyp == "Amazon Betriebsbedarf"):
+        if rechnungstyp == "Amazon Betriebsbedarf":
             konto1 = "4980"
         else:
             konto1 = "4930"
@@ -179,8 +194,7 @@ def post_invoice_data(invoice_data, datum, hinbuchung):
         konto1 = "90000"
         konto2 = "1200"
 
-
-    data_to_post ={
+    data_to_post = {
         "date": datum.strftime('%d.%m.%Y'),
         "rechnungstext": rechnungstyp + f" RN {invoice_data['invoice_number']} {invoice_data['subject']}",
         "betrag": invoice_data['amount'],
@@ -188,61 +202,35 @@ def post_invoice_data(invoice_data, datum, hinbuchung):
         "konto2": konto2
     }
 
-    response = httpx.post(REMOTE_HTTP_URL, json=data_to_post)
+    try:
+        response = httpx.post(REMOTE_HTTP_URL, json=data_to_post)
+    except Exception as e:
+        messagebox.showinfo("Error", f"Unerwarteter Fehler: {str(e)}")
 
     if response.status_code != 201:
-        raise Exception("Failed to post invoice data: " + response.text)
+        messagebox.showinfo("Error", f"Failed to post invoice data: {response.text}")
 
 
-
+# Funktion zur Auswahl der auszuführenden Aktion
 def choose_action(pdf_path, invoice_data):
     """Lässt den Benutzer die gewünschte Aktion auswählen."""
-    action_dialog = tk.Toplevel()
-    action_dialog.title("Aktion auswählen")
+    try:
+        action_dialog = tk.Toplevel()
+        action_dialog.title("Aktion auswählen")
 
-    show_invoice_data_button = tk.Button(
-        action_dialog,
-        text="Rechnungsdaten anzeigen",
-        command=lambda: show_invoice_data(invoice_data))
-    show_invoice_data_button.pack(pady=(0, 3))
+        create_button(action_dialog, "Rechnungsdaten anzeigen", lambda: show_invoice_data(invoice_data))
+        create_button(action_dialog, "Betreff setzen", lambda: set_subject(invoice_data))
+        create_button(action_dialog, "Datei umbenennen", lambda: rename_file(pdf_path, invoice_data))
+        create_button(action_dialog, "Datei hochladen", lambda: move_file(invoice_data))
+        create_button(action_dialog, "Rechnungsdaten senden - Hinbuchung",
+                      lambda: post_invoice_data(invoice_data, invoice_data['date'], 1))
+        create_button(action_dialog, "Rechnungsdaten senden - Rückbuchung",
+                      lambda: post_invoice_data(invoice_data, date.today(), 0))
+        create_button(action_dialog, "Beenden", action_dialog.destroy)
 
-    set_subject_button = tk.Button(
-        action_dialog,
-        text="Betreff setzen",
-        command=lambda: set_subject(invoice_data))
-    set_subject_button.pack(pady=(0, 3))
-
-    rename_file_button = tk.Button(
-        action_dialog,
-        text="Datei umbenennen",
-        command=lambda: rename_file(pdf_path, invoice_data))
-    rename_file_button.pack(pady=(0, 3))
-
-    move_file_button = tk.Button(
-        action_dialog,
-        text="Datei hochladen",
-        command=lambda: move_file(invoice_data))
-    move_file_button.pack(pady=(0, 3))
-
-    post_data_button = tk.Button(
-        action_dialog,
-        text="Rechnungsdaten senden - Hinbuchung",
-        command=lambda: post_invoice_data(invoice_data, invoice_data['date'], 1))
-    post_data_button.pack(pady=(0, 3))
-
-    post_data_button = tk.Button(
-        action_dialog,
-        text="Rechnungsdaten senden - Rückbuchung",
-        command=lambda: post_invoice_data(invoice_data, date.today(), 0))
-    post_data_button.pack(pady=(0, 3))
-
-    quit_button = tk.Button(
-        action_dialog,
-        text="Beenden",
-        command=action_dialog.destroy)
-    quit_button.pack(pady=(0, 3))
-
-    action_dialog.wait_window()
+        action_dialog.wait_window()
+    except Exception as e:
+        messagebox.showinfo("Error", f"Unerwarteter Fehler: {str(e)}")
 
 
 def main(pdf_path):
@@ -265,6 +253,9 @@ def main(pdf_path):
         root.destroy()
     except ValueError as e:
         messagebox.showinfo("Error", f"Fehler: {str(e)}")
+    except Exception as e:
+        messagebox.showinfo("Error", f"Unerwarteter Fehler: {str(e)}")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
